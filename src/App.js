56ba67fbab39f6a0050f1b97f9820a298f6e84c7
Diarse <70977656+NujabesCoder@users.noreply.github.com>
2025-02-15 15:45:22 -0500

    css, done. Lets start the timer logic

--- src/App.js
+++ src/App.js
@@ -16,9 +16,15 @@ function App() {
         Pomodoro timer
       </h1>
       <img src={logo} className="App-logo" alt="logo" />
+
       <header className="App-header">
 
-
+         {/* <span>{{"0"}}</span> */} 
+        <div className="buttons-container">
+          <button className="timers start-btn" title="let's go ! you can do it ! stay focused">start</button>
+          <button className="timers stop-btn"  title="it's okay to take a lil break, come back stronger">stop</button>
+          <button className="timers break-btn" title="congradulation, you deserve it :)">break</button>
+        </div>
         <p>
           Click on the timer if you want to start it. It's a 25 min session with a 5 min break
         </p>
@@ -31,7 +37,7 @@ function App() {
           <span>don't click here</span>
         </a>
 
-        
+
       </header>
     </div>
   );
