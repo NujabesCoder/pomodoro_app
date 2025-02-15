f97df4692dce12189e1705bb58cc58ebf92aeabb
Diarse <70977656+NujabesCoder@users.noreply.github.com>
2025-02-15 15:03:26 -0500

    ajout image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import React, { useState, useEffect } from 'react';
 import logo from './lemon_of_strength.png';
+import lockInImage from './lock_in_Lemon.png';
+import {WORK_TIME, BREAK_TIME} from './constants';
 import './App.css';
+
+
 
 function App() {
   return (
@@ -7,15 +12,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Popka
+          Click on the timer if you want to start it. It's a 25 min session with a 5 min break
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+           className="App-link"
+           href={lockInImage}
+           target="_blank"
+           rel="noopener noreferrer"
         >
-          Learn React
+          <span>don't click here</span>
         </a>
       </header>
     </div>
