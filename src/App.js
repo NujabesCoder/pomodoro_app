07b9f73a792acb38fe0022c594af66ce76d2e64b
Diarse <70977656+NujabesCoder@users.noreply.github.com>
2025-02-16 15:33:32 -0500

    description sur tab

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { useState, useEffect, useRef } from 'react';
 import logo from './lemon_of_strength.png';
 import lockInImage from './lock_in_Lemon.png';
-import {WORK_TIME, BREAK_TIME} from './constants';
+import {WORK_TIME_25, BREAK_TIME_5,} from './constants';
 import breakSoundFinished from './break_finished.wav'
 import sessionSoundFinished from './session_finished.wav'
 import './App.css';
 
 
 function App() {
-  const [timeLeft, setTimeLeft] = useState(WORK_TIME);
+
+  //for settings
+  // const [profile, setProfile] = useState('short');
+
+  //for timer
+  const [timeLeft, setTimeLeft] = useState(WORK_TIME_25);
   const [isRunning, setIsRunning] = useState(false);
   const [onBreak, setOnBreak] = useState(false);
-  const [finishedMessage, setFinishedMessage] = useState("");
-  const [alarmPlayed, setAlarmPlayed] = useState(false);
 
+  //for message
+  const [finishedMessage, setFinishedMessage] = useState("");
+
+  //for alarm
+  const [alarmPlayed, setAlarmPlayed] = useState(false);
   const audioRef = useRef(null);
 
   useEffect(() => {
@@ -23,23 +31,31 @@ function App() {
       timer = setInterval(() => {
         setTimeLeft(prevTime => prevTime - 1);
       }, 1000);
+        
+      !onBreak ? 
+      document.title = `${formatTime(timeLeft)} : Focus time :3` : 
+      document.title = `${formatTime(timeLeft)} : Break time :3`;
+      
     } else if (timeLeft === 0) {
-      setIsRunning(false);
-     if(!alarmPlayed) {
-      if(!onBreak) {
-        audioRef.current = new Audio(sessionSoundFinished);
-        audioRef.current.play();
-        setFinishedMessage("Good job :) go take a break !");
-        setAlarmPlayed(true);
-      } else {
-        audioRef.current = new Audio(breakSoundFinished);
-        audioRef.current.play();
-        setFinishedMessage("Break is Finished ! LOCK IN LOCK IN LOCK IN");
-        setAlarmPlayed(true);
-      }
-     }
-    }
+        !onBreak ? 
+        document.title = `${formatTime(timeLeft)} : Focus time finished :3` : 
+        document.title = `${formatTime(timeLeft)} : Break time finished :3`;
 
+        setIsRunning(false);
+        if(!alarmPlayed) {
+          if(!onBreak) {
+            audioRef.current = new Audio(sessionSoundFinished);
+            audioRef.current.play();
+            setFinishedMessage("Good job :) go take a break !");
+            setAlarmPlayed(true);
+          } else {
+            audioRef.current = new Audio(breakSoundFinished);
+            audioRef.current.play();
+            setFinishedMessage("Break is Finished ! LOCK IN LOCK IN LOCK IN !!!");
+            setAlarmPlayed(true);
+          }
+        }
+    }
     return () => clearInterval(timer);
   }, [isRunning, timeLeft, onBreak, alarmPlayed]);
 
@@ -49,6 +65,7 @@ function App() {
     return `${minutes < 10 ? '0' : ''}${minutes}:${secs < 10 ? '0' : ''}${secs}`;
   };
 
+
   // Event handlers for the buttons
   const handleStart = () => {
     // stop the current audio
@@ -57,9 +74,8 @@ function App() {
       audioRef.current = null;
     }
     setAlarmPlayed(false);
-    //setIsRunning(prev => !prev);
     setOnBreak(false);
-    setTimeLeft(WORK_TIME);
+    setTimeLeft(WORK_TIME_25);
     setIsRunning(true);
     setFinishedMessage("");
   };
@@ -74,9 +90,8 @@ function App() {
       audioRef.current = null;
     }
     setAlarmPlayed(false);
-    //setIsRunning(prev => !prev);
     setOnBreak(true);
-    setTimeLeft(BREAK_TIME);
+    setTimeLeft(BREAK_TIME_5);
     setIsRunning(true);
     setFinishedMessage("");
   };
@@ -87,7 +102,7 @@ function App() {
         Pomodoro timer
       </h1>
       <h1 className="small-title">
-        to help you stay focused :)
+        to help you stay focused :3
       </h1>
       <img src={logo} className="App-logo" alt="logo" />
       
@@ -131,7 +146,7 @@ function App() {
 
         </div>
         <p className="text">
-          Click on <b>"Start"</b> if you want to start the timer. It's a <b>25 min</b> session with a <b>5 min</b> break. If you want to stop the timer press <b>pause</b>, to continue where your left, re-click on the button (but don't abuse it). 
+          Click on <b>"Start"</b> if you want to start the timer. It's a <b>25 min</b> session with a <b>5 min</b> break. If you want to stop the timer press <b>pause</b>, to continue where your left, re-click on it (but don't abuse it). 
         </p>
 
         <a
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
-export const WORK_TIME = 25 * 60;
-export const BREAK_TIME = 5 * 60;
+export const WORK_TIME_25 = 25 * 60;
+export const BREAK_TIME_5 = 5 * 60;
+export const WORK_TIME_50 = 50 * 60;
+export const BREAK_TIME_10 = 10 * 60;
